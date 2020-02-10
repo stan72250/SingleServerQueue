8230903dede45ec9ad1dc30f74b4484a8276b0b6
stan72250 <stan72252@gmail.com>
2020-02-10 09:59:21 -0500

    Add name and title of simulation

--- src/SSQ.java
+++ src/SSQ.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class SSQ {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Stanley Lin");
+		System.out.println("SINGLE SERVER QUEUE SIMULATION");
 	}
 }
